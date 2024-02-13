383f15b8f37750af3b3a13f0e788f7332680b5e6
Yann Le Ravallec <yleravallec@openstudio.fr>
2024-02-13 16:00:16 +0100

    Fonctions avancées sur les tableaux

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
-// Création d'un tableau multidimensionnel pour représenter les membres de l'expédition
 $membresExpedition = [
-    ['nom' => 'Frodon', 'propriétés' => ['Porteur de l\'Anneau', 'Hobbit']],
-    ['nom' => 'Sam', 'propriétés' => ['Jardinier', 'Hobbit']],
-    ['nom' => 'Aragorn', 'propriétés' => ['Protecteur', 'Guide', 'Humain']],
-    ['nom' => 'Gandalf', 'propriétés' => ['Magicien', 'Conseiller']],
-    ['nom' => 'Legolas', 'propriétés' => ['Archer', 'Elfe']],
-    ['nom' => 'Gimli', 'propriétés' => ['Nain', 'Guerrier']],
+    ['nom' => 'Frodon', 'role' => 'Porteur de l\'Anneau', 'age' => 50],
+    ['nom' => 'Sam', 'role' => 'Jardinier et protecteur', 'age' => 38],
+    ['nom' => 'Aragorn', 'role' => 'Protecteur et guide', 'age' => 87],
+    ['nom' => 'Gandalf', 'role' => 'Magicien et conseiller', 'age' => 2019],
+    ['nom' => 'Legolas', 'role' => 'Archer elfe', 'age' => 2931],
+    ['nom' => 'Gimli', 'role' => 'Guerrier nain', 'age' => 139]
 ];
 
-// Accès et affichage des informations de chaque membre
-$i = 0;
+// Tri des membres par âge en utilisant usort et une fonction de rappel
+usort($membresExpedition, static function($a, $b) {
+    // Le « spaceship operator » permet de déterminer si une valeur est supérieure, inférieure ou identique
+    // à une autre valeur. Cela s’exprime par 0 (égal à), -1 (plus petit que), ou 1 (plus grand que).
+    return $a['age'] <=> $b['age'];
+});
+
+// Affichage des membres triés par âge
 foreach ($membresExpedition as $membre) {
-    echo "{$membre['nom']}:";
-    foreach ($membre['propriétés'] as $propriété) {
-        echo " $propriété,";
-        ++$i;
-    }
-    echo '<br><br>';
+    echo "Nom: {$membre['nom']}, Âge: {$membre['age']}<br>";
 }
-echo "Nombre d'iteration : $i";
