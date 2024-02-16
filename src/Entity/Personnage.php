9fdc35fcbe910df42041c253f7f5d9ae27ed4f46
Yann Le Ravallec <yleravallec@openstudio.fr>
2024-02-16 14:53:29 +0100

    Modélisation des données avec Doctrine

---
+++ src/Entity/Personnage.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonnageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonnageRepository::class)]
+class Personnage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $race = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $allegeance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getRace(): ?string
+    {
+        return $this->race;
+    }
+
+    public function setRace(string $race): static
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    public function getAllegeance(): ?string
+    {
+        return $this->allegeance;
+    }
+
+    public function setAllegeance(?string $allegeance): static
+    {
+        $this->allegeance = $allegeance;
+
+        return $this;
+    }
+}
