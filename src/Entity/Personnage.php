02fef8ab217a81e050a35611737d39e453254d9e
Yann Le Ravallec <yleravallec@openstudio.fr>
2024-02-19 10:35:21 +0100

    Créer une APIResource à partir d’une entité

--- src/Entity/Personnage.php
+++ src/Entity/Personnage.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
+#[ApiResource]
 class Personnage {
     #[ORM\Id]
     #[ORM\GeneratedValue]
