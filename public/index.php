6f63bda66c7849f6325b50a822b870fa4cb800c0
Le Ravallec <B098005@zcam.ztech>
2024-02-22 11:55:35 +0100

    Structures de contrôle

--- public/index.php
+++ public/index.php
@@ -1,22 +1,40 @@
 <?php
 
-$personnage     = 'Frodo';
-$anneeNaissance = 2968;
-$taille         = 107.9;
-$estUnHobbit    = true;
+/** @var Array<array<string>> $personnages */
+$personnages = [
+    ['nom' => 'Frodo', 'race' => 'Hobbit', 'allégeance' => 'La Comté'],
+    ['nom' => 'Aragorn', 'race' => 'Humain', 'allégeance' => 'Gondor'],
+    ['nom' => 'Legolas', 'race' => 'Elfe', 'allégeance' => 'Forêt Noire'],
+    ['nom' => 'Gimli', 'race' => 'Nain', 'allégeance' => 'Moria'],
+    ['nom' => 'Gandalf', 'race' => 'Istar', 'allégeance' => 'Les peuples libres'],
+];
 
-echo "Nom du personnage : $personnage <br>";
-echo "Annee de naissance : $anneeNaissance <br>";
-echo "Taille : $taille <br>";
-echo 'Est-ce un hobbit ? ';
-
-echo $estUnHobbit ? 'Oui' : 'Non';
-
-$taille = 102.5;
+$nomRecherche = 'Gandalf';
+foreach ($personnages as $nom => $personnage) {
+    switch ($personnage['nom']) {
+        case $nomRecherche :
+        {
+            echo " $nomRecherche allège à {$personnage['allégeance']}";
+            echo '<br>';
+            break 2;
+        }
+        case 'Frodo' :
+        {
+            echo "C'est le porteur de l'anneau";
+            echo '<br>';
+            break;
+        }
+        default:
+            echo "Je passe dans mon switch <br>";
+            break;
+    }
+}
 
 echo "<br>";
-echo "Nouvelle taille : $taille cm";
+foreach ($personnages as $nom => $personnage) {
+    echo " $nom est de la race des {$personnage['race']} et allège à {$personnage['allégeance']}";
+    echo "<br>";
+}
 
-$nomComplet = $personnage . ' Sacquet';
-echo "<br>";
-echo "Nom complet: $nomComplet";
+
+
