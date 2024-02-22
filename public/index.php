26666686786cf81b068b6d270394e55fc77e05a4
Le Ravallec <B098005@zcam.ztech>
2024-02-22 14:29:53 +0100

    Définition et utilisation des fonctions

--- public/index.php
+++ public/index.php
@@ -1,40 +1,19 @@
 <?php
 
-/** @var Array<array<string>> $personnages */
-$personnages = [
-    ['nom' => 'Frodo', 'race' => 'Hobbit', 'allégeance' => 'La Comté'],
-    ['nom' => 'Aragorn', 'race' => 'Humain', 'allégeance' => 'Gondor'],
-    ['nom' => 'Legolas', 'race' => 'Elfe', 'allégeance' => 'Forêt Noire'],
-    ['nom' => 'Gimli', 'race' => 'Nain', 'allégeance' => 'Moria'],
-    ['nom' => 'Gandalf', 'race' => 'Istar', 'allégeance' => 'Les peuples libres'],
-];
-
-$nomRecherche = 'Gandalf';
-foreach ($personnages as $nom => $personnage) {
-    switch ($personnage['nom']) {
-        case $nomRecherche :
-        {
-            echo " $nomRecherche allège à {$personnage['allégeance']}";
-            echo '<br>';
-            break 2;
-        }
-        case 'Frodo' :
-        {
-            echo "C'est le porteur de l'anneau";
-            echo '<br>';
-            break;
-        }
-        default:
-            echo "Je passe dans mon switch <br>";
-            break;
-    }
+function role($nomPersonnage)
+{
+    return match ($nomPersonnage) {
+        'Frodon' => 'Porteur de l\'anneau',
+        'Aragorn' => 'Guide et futur roi du gondor',
+        'Gandalf' => 'Magicien',
+        'Gimli' => 'Guerrier nain',
+        'Legolas' => 'Archer',
+        default => 'Personnage inconnu'
+    };
 }
 
-echo "<br>";
-foreach ($personnages as $nom => $personnage) {
-    echo " $nom est de la race des {$personnage['race']} et allège à {$personnage['allégeance']}";
-    echo "<br>";
+$personnages = ['Frodon', 'Sam', 'Aragorn', 'Gandalf', 'Legolas', 'Gimli'];
+
+foreach ($personnages as $personnage) {
+    echo "$personnage est " . role($personnage) . '<br>';
 }
-
-
-
