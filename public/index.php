062a2675a3bb57e9ff46b178a264627b6a477b42
Le Ravallec <B098005@zcam.ztech>
2024-02-26 10:20:23 +0100

    Tri de tableau

--- public/index.php
+++ public/index.php
@@ -2,23 +2,23 @@
 
 
 $membresExpedition = [
-    ['nom' => 'Frodo', 'propriété' => ['Porteur de l\'anneau', 'Hobbit']],
-    ['nom' => 'Sam', 'propriété' => ['Jardinier', 'Hobbit']],
-    ['nom' => 'Aragorn', 'propriété' => ['Protecteur', 'Humain']],
-    ['nom' => 'Gandalf', 'propriété' => ['Magicien', 'Conseiller']],
-    ['nom' => 'Legolas', 'propriété' => ['Archer', 'Elfe']],
-    ['nom' => 'Gimli', 'propriété' => ['Nain', 'Guerrier']],
+    ['nom' => 'Frodo', 'propriété' => ['Porteur de l\'anneau', 'Hobbit'], 'age' => 50],
+    ['nom' => 'Sam', 'propriété' => ['Jardinier', 'Hobbit'], 'age' => 38],
+    ['nom' => 'Aragorn', 'propriété' => ['Protecteur', 'Humain'], 'age' => 87],
+    ['nom' => 'Gandalf', 'propriété' => ['Magicien', 'Conseiller'], 'age' => 2019],
+    ['nom' => 'Legolas', 'propriété' => ['Archer', 'Elfe'], 'age' => 2931],
+    ['nom' => 'Gimli', 'propriété' => ['Nain', 'Guerrier'], 'age' => 139],
 ];
 
+usort($membresExpedition, static function ($a, $b) {
+    //Spaceship operator
+    return $a['age'] <=> $b['age'];
+});
 
-$i = 0;
+
+//Affichage des membres de l'expedition trié par âge
 foreach ($membresExpedition as $membre) {
-    echo "{$membre['nom']} : ";
-    foreach ($membre['propriété'] as $propriete) {
-        echo "$propriete, ";
-        ++$i;
-    }
-    echo '<br>';
+    echo "Nom : {$membre['nom']}, Âge: {$membre['age']} <br>";
 }
 
-echo "Nombre d'itération : $i";
+
